d679d1786991f91da42e15eaad8b6e39e24319b9
ulex <alexander.ulitin@ya.ru>
2013-05-23 01:12:33 +0400

    merge with small nout

--- presentation/plot_psnr_all.m
+++
@@ -1,6 +0,0 @@
-load('psnr_all.mat');
-close;
-x = 1:length(psnr_bicubic);
-title('PSNR for all')
-plot(x, [psnr_sr2; psnr_bicubic; psnr_nearest;psnr_sr1]);
-legend('psnr SR_2', 'psnr bicubic', 'psnr nearest', 'psnr SR_1');
